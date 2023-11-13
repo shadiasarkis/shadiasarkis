e3cb4e68d57f368f575d55764f7198083ba04d80
shadiasarkis <ssarkisb@udd.cl>
2023-11-13 00:34:44 -0300

    Certamen 2

--- themes/lib/main.dart
+++ themes/lib/main.dart
@@ -74,23 +74,138 @@ class _HomeAppState extends State<HomeApp> with SingleTickerProviderStateMixin {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: TabBarView(
-              controller: _tabController,
-              children: const [
-                Center(
-                  child: Text('Pestaña de Grupos'),
+
+      body: SingleChildScrollView(
+        //Permite eventualemnte desplazarse a un punto x en el scroll.
+
+        child: Column(
+          //será una columna vertical, navegamos verticalmente
+
+          children: [
+            Container(
+              //superficie azul, de acá en adelante todo será un container
+              color: const Color(0xFF023047), // Color en formato hexadecimal
+              height: 300.0, //alto
+              width: double.infinity, //ancho, al infinito y más allá
+            ),
+
+            const Divider(
+              //para separar del siguiente container, es horizontal
+              height: 4.0, //dos pixeles de alto
+              color: Color.fromARGB(255, 255, 255, 255), //color
+            ),
+
+            Row(
+              //una fila que tiene dos columnas, separadas por una línea, alto 48 píxeles
+              children: [
+                Expanded(
+                  //cada expanded es una columna al interior del container, se construye el container en su interior
+                  flex: 1, //primera división de la superficier, ROW
+                  child: Container(
+                    // acá se construye, el contenido de esa superficie previamente formateada.
+                    height: 48.0, //con alto de 48 pixeles
+                    color:
+                        const Color(0xFF8EDBCE), // Color en formato hexadecimal
+                  ),
                 ),
-                Center(
-                  child: Text('Pestaña de Chats'),
+
+                const SizedBox(width: 4.0), //separador vertical
+
+                Expanded(
+                  flex: 1, //segunda división de la superficie, ROW
+                  child: Container(
+                    height: 48.0,
+                    color:
+                        const Color(0xFF8EDBCE), // Color en formato hexadecimal
+                  ),
                 ),
               ],
             ),
+
+            const Divider(
+              //Nuevo separador horizontal
+              height: 4.0,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+
+            //Nueva declaración de fila, tendrá 4 subdivisiones
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 96.0,
+                    color:
+                        const Color(0xFFE9DDFF), // Color en formato hexadecimal
+                  ),
+                ),
+
+                const SizedBox(width: 4.0), //separación vertical
+
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 96.0,
+                    color:
+                        const Color(0xFFE9DDFF), // Color en formato hexadecimal
+                  ),
+                ),
+
+                const SizedBox(width: 4.0), //separación vertical
+
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 96.0,
+                    color:
+                        const Color(0xFFFB6900), // Color en formato hexadecimal
+                  ),
+                ),
+
+                const SizedBox(width: 4.0), //separación vertical
+
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 96.0,
+                    color:
+                        const Color(0xFFFB6900), // Color en formato hexadecimal
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ), // Se cierra el body
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: MyTheme.lightTheme().colorScheme.primaryContainer,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white), // Icono blanco
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: Colors.white), // Icono blanco
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_outlined,
+                color: Colors.white), // Icono blanco
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon:
+                Icon(Icons.email_outlined, color: Colors.white), // Icono blanco
+            label: '',
           ),
         ],
+        selectedItemColor:
+            Colors.white, // Color del texto seleccionado en blanco
+        unselectedItemColor: const Color.fromARGB(
+            255, 103, 102, 102), // Color de íconos no seleccionados
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (kDebugMode) {
